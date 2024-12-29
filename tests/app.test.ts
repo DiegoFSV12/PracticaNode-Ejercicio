f73585184cc82880ade1d2d90ba7b575205ef4c9
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2024-12-29 14:15:47 -0500

    Testing configurado

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,11 @@
-describe('App', () => {
-    it('should be true',()=>{
-        expect(true).toBe(true);
+describe('Test in the App file', () => {
+    it('should be 30',()=>{
+        // Arrange
+        const num1 = 10;
+        const num2 = 20;
+        // Act
+        const result = num1 + num2;
+        // Assert
+        expect(result).toBe(30);
     });
 });
